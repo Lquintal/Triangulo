23ee08338819e21cde52882e916f2c47f8454539
Lquintal <WichOob.boy@gmail.com>
2017-02-16 09:24:35 -0600

    Update Triangulo.java

--- app/src/main/java/com/example/lquintal/triangulo/Triangulo.java
+++ app/src/main/java/com/example/lquintal/triangulo/Triangulo.java
@@ -3,6 +3,7 @@ package com.example.lquintal.triangulo;
 /**
  * Created by l.quintal on 14/02/2017.
  */
+/*Prueba de edicion*/
 public class Triangulo {
     public abstract class activity_main{
         public abstract double area();
